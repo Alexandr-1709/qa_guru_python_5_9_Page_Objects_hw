59b75d44b7214df0bd1a427163313324f3a81f6b
Aleksandr <nikiforov1709@ya.ru>
2023-05-14 12:51:05 +0500

    Added User class

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,6 +1,7 @@
 import os
 from selene.support.shared import browser
 from selene import have, command
+from data.users import User
 
 
 class RegistrationFormPage:
@@ -8,54 +9,58 @@ class RegistrationFormPage:
     def open(self):
         browser.open('/automation-practice-form')
 
-    def fill_in_first_name(self, value):
-        browser.element('#firstName').type(value)
+    def fill_form(self, student: User):
+        browser.element('#firstName').type(student.first_name)
+        browser.element('#lastName').type(student.last_name)
 
-    def fill_in_last_name(self, value):
-        browser.element('#lastName').type(value)
+        browser.element('#userEmail').type(student.email)
 
-    def fill_in_email(self, value):
-        browser.element('#userEmail').type(value)
+        browser.all('[name=gender]').element_by(have.value(f'{student.gender.Male.value}')).double_click()
 
-    def select_gender(self, value):
-        browser.all('[name=gender]').element_by(have.value(f'{value}')).double_click()
+        browser.element('#userNumber').type(student.phone_number)
 
-    def fill_in_phone_number(self, value):
-        browser.element('#userNumber').type('9009997733')
-
-    def fill_in_date_of_birth(self, day, month, year):
         browser.element('#dateOfBirthInput').press()
-        browser.element('.react-datepicker__month-select').send_keys(month)
-        browser.element('.react-datepicker__year-select').send_keys(year)
-        browser.element(f'.react-datepicker__day--0{day}').click()
+        browser.element('.react-datepicker__month-select').send_keys(student.birth_month)
+        browser.element('.react-datepicker__year-select').send_keys(student.birth_year)
+        browser.element(f'.react-datepicker__day--0{student.birth_day}').click()
 
-    def fill_in_subjects(self, value):
-        browser.element('#subjectsInput').type(value).press_enter()
+        for subject in student.subjects:
+            browser.element('#subjectsInput').type(subject.value).press_enter()
 
-    def select_hobby(self, value):
-        browser.all('[for^=hobbies-checkbox]').element_by(have.exact_text(value)).click()
+        for hobby in student.hobbies:
+            browser.all('[for^=hobbies-checkbox]').element_by(have.exact_text(hobby.value)).click()
 
-    def upload_picture_file(self, value):
         browser.element('#uploadPicture').send_keys(os.path.abspath(
-            os.path.join(os.path.dirname(__file__), os.path.pardir, f'resourсes/{value}')))
+            os.path.join(os.path.dirname(__file__), os.path.pardir, f'resourсes/{student.picture_file}')))
 
-    def fill_in_current_address(self, value):
-        browser.element('#currentAddress').type(value)
+        browser.element('#currentAddress').type(student.address)
 
-    def select_state(self, value):
         browser.element('#state').perform(command.js.scroll_into_view)
         browser.element('#state').click()
-        browser.all('[id^=react-select][id*=option]').element_by(have.exact_text(value)).click()
+        browser.all('[id^=react-select][id*=option]').element_by(have.exact_text(student.state)).click()
 
-    def select_city(self, value):
-        browser.element('#react-select-4-input').type(value).press_enter()
+        browser.element('#react-select-4-input').type(student.city).press_enter()
 
-    def submit_form(self):
         browser.element('#submit').press_enter()
 
-    def should_registered_user_with(self, *args):
-        browser.element('.table').all('td').even.should(have.texts(args))
+    def should_registered_user_with(self, student: User):
+        full_name = f'{student.first_name} {student.last_name}'
+        day_of_birth = f'{student.birth_day} {student.birth_month},{student.birth_year}'
+        state_and_city = f'{student.state} {student.city}'
+        subjects = ', '.join([subject.value for subject in student.subjects])
+        hobbies = ', '.join([hobby.value for hobby in student.hobbies])
+        browser.element('.table').all('td').even.should(have.texts(full_name,
+                                                                   student.email,
+                                                                   student.gender.Male.value,
+                                                                   student.phone_number,
+                                                                   day_of_birth,
+                                                                   subjects,
+                                                                   hobbies,
+                                                                   student.picture_file,
+                                                                   student.address,
+                                                                   state_and_city
+                                                                   )
+                                                        )
 
     def close_submit_form(self):
         browser.element('#closeLargeModal').press_enter()
-
--- tests/test_student_registration_form.py
+++ tests/test_student_registration_form.py
@@ -1,50 +1,34 @@
-import os
-from selene.support.shared import browser
-from selene import have, command
 from pages.registration_page import RegistrationFormPage
+from data.users import Hobby, Gender, User, Subject
+
 
 
 def test_practice_form(browser_management):
     registration_page = RegistrationFormPage()
 
+    student = User(
+        first_name='Aleksandr',
+        last_name='Nikiforov',
+        email='nikiforov@mail.ru',
+        gender=Gender.Male,
+        phone_number='9009997733',
+        birth_day= '17',
+        birth_month='September',
+        birth_year='1982',
+        subjects=[Subject.computer_science],
+        hobbies=[Hobby.sports, Hobby.music],
+        picture_file='test_5_5.jpg',
+        address='Moscow, Vernadsky avenue,19',
+        state='NCR',
+        city='Delhi',
+    )
+
     registration_page.open()
 
     # WHEN
-    registration_page.fill_in_first_name('Aleksandr')
-    registration_page.fill_in_last_name('Nikiforov')
-
-    registration_page.fill_in_email('nikiforov@mail.ru')
-
-    registration_page.select_gender('Male')
-
-    browser.element('#userNumber').type('9009997733')
-
-    registration_page.fill_in_date_of_birth('17', 'September', '1982')
-
-    browser.element('#subjectsInput').type("Computer Science").press_enter()
-
-    registration_page.select_hobby('Sports')
-    registration_page.select_hobby('Music')
-
-    registration_page.upload_picture_file('test_5_5.jpg')
-
-    registration_page.fill_in_current_address('Moscow, Vernadsky avenue,19')
-
-    registration_page.select_state('NCR')
-    registration_page.select_city('Delhi')
-
-    registration_page.submit_form()
+    registration_page.fill_form(student)
 
     # THEN
-    registration_page.should_registered_user_with('Aleksandr Nikiforov',
-                                                  'nikiforov@mail.ru',
-                                                  'Male',
-                                                  '9009997733',
-                                                  '17 September,1982',
-                                                  'Computer Science',
-                                                  'Sports, Music',
-                                                  'test_5_5.jpg',
-                                                  'Moscow, Vernadsky avenue,19',
-                                                  'NCR Delhi')
+    registration_page.should_registered_user_with(student)
 
     registration_page.close_submit_form()
